69f93c3e7f51d01e51f3d22385cbef4b1282522f
Joe Wise <joedavidwise@gmail.com>
2016-11-04 15:20:29 -0700

    Bringing in the old project

---
+++ Assets/Lights/Scripts/LanternManager.cs
@@ -0,0 +1,247 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handles lantern events. Light pickups and lantern flares.
+public class LanternManager : MonoBehaviour {
+
+	// Burst attack
+	public float cooldown = 1.5f; // Number of seconds between allowed shots
+									// NEED TO PLAYTEST THIS VALUE
+
+	public static int ammunition = 0; // Ammunition currently available - always updated
+									// Accessed in enemy AI script to determine a valid lantern attack
+
+	// Light pickups
+	public static bool pickingUpAudioTrigger = false; // Triggers music change in MusicManager
+
+	// Audio
+	public AudioSource[] lantern_audio_source;
+	public AudioSource lantern_audio_source_stun_sweetner;
+	public AudioClip[] lantern_audio_clips_stun_sweetner;
+	//public AudioClip[] burstSounds; // Sounds on lantern shot (1-3 shots, 0 empty)
+
+	// Burst mechanic
+	private float lightBurstLifetime = 1.0f;
+	private int numBurst2Particles = 1; // Num particles for lantern burst
+	private int numBurst3Particles = 10; // Num particles for lantern wind effect burst
+
+	// Lantern centres and game state
+	public GameObject lanternYellow, lanternBlue, lanternGreen, lanternRed;
+	private GameObject[] lanternContents; // In enum order - lantern colour prefabs
+	private ParticleSystem[] lightBurst2; // Ring effect
+	private ParticleSystem[] lightBurst3; // Wind effect
+	private GameObject[] burstParticleContainer; // For overall rotation
+	private bool[] lanternContentsUnlocked; // Which lights are currently unlocked?
+	private bool[] lanternContentsAvailable; // Which lights are currently ready for use?
+	public static string lightToAddName = null;
+
+	public enum Colours {Yellow = 0, Blue, Green, Red};
+
+	/* Used for narration, do not remove */
+	private bool has_triggered_narration = false;
+
+
+	// Use this for initialization
+	void Start () {
+		// Initialise lantern contents
+		lanternContents = new GameObject[4];
+		lanternContents [(int)Colours.Yellow] = lanternYellow;
+		lanternContents [(int)Colours.Blue] = lanternBlue;
+		lanternContents [(int)Colours.Green] = lanternGreen;
+		lanternContents [(int)Colours.Red] = lanternRed;
+		// Start with empty lantern
+		lanternContentsUnlocked = new bool[]{false, false, false, false};
+		lanternContentsAvailable = new bool[]{false, false, false, false};
+
+		// Put light burst effects into memory
+		lightBurst2 = new ParticleSystem[4];
+		lightBurst3 = new ParticleSystem[4];
+		burstParticleContainer = new GameObject[4]; // For overall rotation
+		for (int i = 0; i < 4; i++) {
+			burstParticleContainer [i] = new GameObject();
+			burstParticleContainer [i] = lanternContents [i].transform.FindChild ("animation_shell"). 
+				FindChild ("burstParticles").gameObject;
+			if (lightBurst2 [i] = null)
+				Debug.Log ("problem with burstParticleContainer");
+
+			GameObject burstParticlesObj = lanternContents [i].transform.FindChild ("animation_shell").
+				FindChild ("burstParticles").gameObject;
+
+			Transform[] children = burstParticlesObj.GetComponentsInChildren<Transform> ();
+			foreach (Transform child in children) {
+				if (child.name == "burst2") {
+					lightBurst2 [i] = child.GetComponent<ParticleSystem> ();
+					//Debug.Log ("Found child: " + child.name);
+				} else if (child.name == "burst3") {
+					lightBurst3 [i] = child.GetComponent<ParticleSystem> ();
+					//Debug.Log ("Found child: " + child.name);
+				} else {
+					//Debug.Log ("Found unexpected child in burstParticles: " + child.name);
+				}
+			}
+			if (lightBurst2 [i] == null)
+				Debug.Log ("problem with lightBurst2");
+			if (lightBurst3 [i] == null)
+				Debug.Log ("problem with lightBurst3");
+		}
+
+		// Set all to inactive at first
+		for (int i = 0; i < 4; i++) {
+			lanternContents [i].SetActive (false);
+		}
+
+		// Audio
+
+		//lantern_audio_source.clip = burstSounds [0]; // Default
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// If send message from light source (collected)
+		if (lightToAddName != null) {
+			addLight (lightToAddName);
+			//////////////////////////////////////////
+			if (has_triggered_narration == false) {
+				has_triggered_narration = true;
+				MusicManager.light_name = lightToAddName;
+			}
+			/////////////////////////////////////////
+			lightToAddName = null;
+		}
+
+		if (Input.GetButtonDown("Interact")) {
+			int index = availableLight ();
+			if (index >= 0) {
+				int random = Random.Range (1, lantern_audio_source.Length);
+				lantern_audio_source[random].loop = false;
+				lantern_audio_source[random].Play ();
+				lantern_audio_source_stun_sweetner.clip = lantern_audio_clips_stun_sweetner[Random.Range(1, lantern_audio_clips_stun_sweetner.Length)];
+				lantern_audio_source_stun_sweetner.Play ();
+
+				Debug.Log ("Found available light in slot " + index);
+				burstHandle (index);
+
+				Debug.Log ("Disabling light " + index);
+				lanternContentsAvailable [index] = false; // Disable burst ability
+				StartCoroutine (lightBurstCooldown (index)); // Enable after cooldown
+			} else {
+				// Play empty
+				lantern_audio_source[0].loop = false;
+				lantern_audio_source[0].Play ();
+			}
+		}
+	}
+
+	int availableLight() {
+		for(int i = 0; i < 4; i++) {
+			if(lanternContentsUnlocked[i] && lanternContentsAvailable[i])
+				return i;
+		}
+		return -1; // None available
+	}
+
+	void burstHandle(int index) {
+		resetParticleRotation (burstParticleContainer[index]);
+		burst2 (lightBurst2[index]); // Centre circle
+		burst3 (lightBurst3[index]); // Wind
+	}
+
+	void resetParticleRotation(GameObject burstParticleContainer) {
+		burstParticleContainer.transform.eulerAngles = new Vector3 (90.0f, 0.0f, 0.0f);
+	}
+
+	// Again this can be done with 1 function and an array once I know all the parameters
+	void burst2(ParticleSystem lightBurst) {
+		lightBurst.Emit (numBurst2Particles);
+	}
+
+	void burst3(ParticleSystem lightBurst) {
+		lightBurst.Emit (10);
+	}
+
+	void addLight(string toAdd) {
+
+		int indexToAdd = findLightIndex (toAdd);
+
+		Debug.Log ("Enabling light (from void addLight) " + indexToAdd);
+
+		lanternContentsUnlocked [indexToAdd] = true;
+		lanternContentsAvailable [indexToAdd] = true;
+		ammunition++;
+		lanternContents [indexToAdd].SetActive (true); // Change to animation
+	}
+
+	int findLightIndex(string toAdd) {
+		if (toAdd == "LightPickup_yellow") {
+			//Debug.Log ("adding yellow");
+			return (int)Colours.Yellow;
+		} else if (toAdd == "LightPickup_blue") {
+			//Debug.Log ("adding blue");
+			return (int)Colours.Blue;
+		} else if (toAdd == "LightPickup_green") {
+			//Debug.Log ("adding green");
+			return (int)Colours.Green;
+		} else if (toAdd == "LightPickup_red") {
+			//Debug.Log ("adding red");
+			return (int)Colours.Red;
+		} else {
+			Debug.Log ("Error parsing light pickup name");
+			return 0;
+		}
+	}
+
+	IEnumerator lightBurstCooldown(int index) { //Add cooldown timer to lantern light burst
+		yield return new WaitForEndOfFrame();
+		ammunition--;
+
+
+		// Disable emission
+		Transform emissiveSphere = lanternContents [index].transform.FindChild ("animation_shell").
+			FindChild ("centre");
+		emissiveSphere.localScale /= 2.0f;
+		Behaviour halo = (Behaviour)emissiveSphere.gameObject.GetComponent ("Halo");
+		halo.enabled = false;
+		//Renderer renderer = lanternContents [index].transform.FindChild ("animation_shell").
+		//	FindChild ("centre").GetComponent<Renderer> ();
+		//Color materialColour = renderer.material.color;
+		//DynamicGI.SetEmissive (renderer, materialColour * 0.1f);
+
+		// halo false
+
+
+		Debug.Log ("Cooldown started");
+
+		// Wait
+		yield return new WaitForSeconds (cooldown);
+
+		emissiveSphere.localScale *= 2.0f;
+		halo.enabled = true;
+
+		Debug.Log ("Cooldown ended");
+		// Enable
+		// Enable emission
+		//DynamicGI.SetEmissive(renderer, materialColour * 2.5f);
+		//halo true
+		Debug.Log ("Enabling light " + index);
+		lanternContentsAvailable[index] = true;
+		ammunition++;
+		Debug.Log ("Remaining ammunition: " + ammunition);
+	}
+
+	/*
+	void updateLantern(GameObject otherObject)
+	{
+		Color toAdd = otherObject.GetComponent<ParticleSystem> ().startColor;
+		for (int i = 0; i < 3; i++)
+		{
+			//Debug.Log("Colour: " + i + ": " + toAdd[i]);
+			if (toAdd[i] < 0.9) toAdd[i] = 0; // Remove secondary colours
+		}
+		Color toDecrease = new Color(0.0f, 0.0f, 0.0f);
+		//lanternCentre.startColor -= toDecrease;
+		//lanternCentre.startColor += toAdd / 3;
+		//lanternLight.range *= 1.1f;
+		//lanternLight.intensity *= 1.1f;
+	}*/
+}
---
+++ Assets/Lights/Scripts/PickUpLight.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpLight : MonoBehaviour {
+
+	public GameObject lanternCentre;
+	public ParticleSystem dust; // Moves to player
+	public ParticleSystem explosion; // Explosion effect
+
+	public ParticleSystem lanternSwirl; // Dust swirls around lantern to trigger (yel, blu, gre, red - enum from lanternManager)
+	private ParticleSystem.Particle[] lanternSwirlParticles; // Temp array for calculating new dust positions
+
+	private Transform lanternCentreTrans;
+	private ParticleSystem.Particle[] particles; // Temp array for calculating new dust positions
+
+	// Particle dust to player lantern effect
+	private float speedDuringAnim = 5.0f; // Speed to keep particles circling player
+	private float speedEndAnim = 10.0f;	// Speed into lanten at end
+	private float decel = 1.00f/*0.96f*/; // Need to decelerate particles to remove their initial velocity
+	// and accurately stream to target
+
+	private bool playingAnim = false; // Is animation playing?
+	private bool emittingParticles = true; // Are there still particles emitting? For when they should be stopped
+
+	// Animation points
+	private float climaxTime = 6.0f;
+	private float endingTime = 10.0f;
+	private float swirlTime = 0.2f; // Time to start swirl around lantern after trigger
+
+	private float timeAnimStart;
+
+	// Use this for initialization
+	void Start () {
+		lanternCentreTrans = lanternCentre.transform;
+		if(dust != null)
+		if (particles == null || particles.Length < dust.maxParticles)
+			particles = new ParticleSystem.Particle[dust.maxParticles]; 
+
+		if(lanternSwirl != null)
+		if (lanternSwirlParticles == null || lanternSwirlParticles.Length < lanternSwirl.maxParticles)
+			lanternSwirlParticles = new ParticleSystem.Particle[lanternSwirl.maxParticles]; 
+
+		//lanternSwirl.SetActive (false); // Turn off dust swirl around lantern
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void pickUp() {
+		//Debug.Log ("Pick up sigal received");
+		pickUpLight (this.gameObject);
+	}
+
+	void pickUpLight(GameObject light) {
+		//Debug.Log ("Picking up the thing\n");
+		playingAnim = true; // Start dust towards player anim
+
+		StartCoroutine("moveLightToPlayer");
+
+		pickUpAnimation (light); // Start light burst anim
+
+		light.transform.parent.GetComponent<AudioSource>().Play(); // Trigger sound
+		LanternManager.pickingUpAudioTrigger = true; // Trigger music
+
+		// Need to change - should probably access var in a GameManager instead
+		//gameState++;
+	}
+
+	void pickUpAnimation(GameObject light) {
+		// Start Unity-build animation for light source
+		light.GetComponent<Animation>().Play ();
+
+		// Turn off beacons
+		Transform[] children = light.GetComponentsInChildren<Transform> ();
+		foreach (Transform child in children) {
+			if (child.gameObject.CompareTag ("Beacon")) {
+				var temp = child.gameObject.GetComponent<ParticleSystem> ().emission;
+				temp.enabled = false;
+				//Debug.Log ("Found child");
+			}
+		}
+
+		//updateLantern (light);
+	}
+
+	IEnumerator moveLightToPlayer() {
+		timeAnimStart = Time.time; // Time animation initialised
+		bool triggeredAdd = false;
+		bool triggeredLanternSwirl = false;
+
+		// Triggered: make faster; decrease lifetime; spawn more?
+		int numParticles = dust.GetParticles (particles);
+		int numLanternDustParticles = lanternSwirl.GetParticles (lanternSwirlParticles);
+		for (int i = 0; i < numParticles; i++) {
+			particles [i].startLifetime = 3.2f; // Do not allow to linger
+		}
+
+		while (playingAnim) {
+			numParticles = dust.GetParticles (particles);
+			numLanternDustParticles = lanternSwirl.GetParticles (lanternSwirlParticles);
+
+			if (Time.time - timeAnimStart < climaxTime) { // During animation
+				for (int i = 0; i < numParticles; i++) {
+					particles [i].velocity *= 0.0f; // remove circular motion
+					particles [i].position = Vector3.Lerp (
+						particles [i].position, lanternCentreTrans.position, 
+						speedDuringAnim * (Time.deltaTime));
+					// If too close to lantern, kill
+					if (Vector3.Distance (particles[i].position, lanternCentreTrans.position) < 0.25f)
+						particles [i].lifetime = 0.0f;
+				}
+				dust.SetParticles (particles, numParticles);
+
+			} else { // End of animation -- push quickly into lantern
+				// Dust towards lantern from light source
+				for (int i = 0; i < numParticles; i++) {
+					particles [i].position = Vector3.Lerp (
+						particles [i].position, lanternCentreTrans.position, 
+						speedEndAnim * (Time.deltaTime));
+					particles [i].startLifetime = 0.2f; // Do not allow to linger
+				}
+				dust.SetParticles (particles, numParticles);
+
+				// Dust swirl around lantern
+				Vector3 LocalLantCentre = lanternSwirl.transform.localPosition; // These are rendered in local space - need to reverse pos for orirgin
+				// This doesn't work though.
+				// Keep trying next week.
+
+
+				Debug.Log ("pos: " + LocalLantCentre.x/* + ", " + LocalLantCentre.y, + ", " + LocalLantCentre.z*/);
+				for (int i = 0; i < numLanternDustParticles; i++) {
+					lanternSwirlParticles [i].position = Vector3.Lerp (
+						lanternSwirlParticles [i].position, LocalLantCentre, 
+						speedEndAnim * (Time.deltaTime));
+					lanternSwirlParticles [i].startLifetime = 0.2f; // Do not allow to linger
+				}
+				lanternSwirl.SetParticles (lanternSwirlParticles, numLanternDustParticles);
+
+				if(emittingParticles) {
+					stopEmission(); // Stop producing particles
+					explode (); // Final burst (explosion)
+				}
+
+				// Trigger addition to lantern
+				if (triggeredAdd == false) {
+					triggeredAdd = true;
+					LanternManager.lightToAddName = this.transform.parent.gameObject.name;
+				}
+
+			}
+
+			// Start swirl around lantern
+			if (Time.time - timeAnimStart < swirlTime && triggeredLanternSwirl == false) { 
+				lanternSwirl.Play ();
+				triggeredLanternSwirl = true;
+			}
+
+			if (Time.time - timeAnimStart > endingTime) // Stop coroutine
+				playingAnim = false;
+
+			yield return null;
+
+		}
+		terminateAnimation ();
+		yield return null;
+
+	}
+
+	void explode() {
+		burst ();
+		Invoke ("burst", 0.05f);
+		Invoke ("burst", 0.1f);
+	}
+
+	void burst() {
+		explosion.Emit (1);
+	}
+
+	// Stop particle emitters (allow them to fade)
+	void stopEmission() {
+		emittingParticles = false;
+
+		ParticleSystem[] children = this.GetComponentsInChildren<ParticleSystem> ();
+		foreach (ParticleSystem child in children) {
+			var emit = child.emission;
+			emit.enabled = false;
+		}
+		// & parent
+		var temp = this.GetComponent<ParticleSystem> ().emission;
+		temp.enabled = false;
+
+		temp = lanternSwirl.emission;
+		temp.enabled = false;
+	}
+
+	// Delete gameObject
+	void terminateAnimation() {
+		this.transform.parent.gameObject.SetActive (false);
+		lanternSwirl.transform.parent.gameObject.SetActive (false);
+	}
+
+}
