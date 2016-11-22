9057eb3564159ae4a76c770ee9aff70765dd02d8
Ryan Garigliano <Ryan@Ryans-MacBook-Pro.local>
2016-11-22 00:57:06 -0800

    Sound: - footsteps for double stacked freggo

---
+++ Assets/playFootstepSound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playFootstepSound : MonoBehaviour {
+
+	public AudioClip[] footsteps;
+
+	void FootstepSound(){
+		var audio_source = GetComponentInParent<AudioSource>();
+		audio_source.clip = footsteps[Random.Range(0, (footsteps.Length-1))];
+		audio_source.Play ();
+		}
+}
