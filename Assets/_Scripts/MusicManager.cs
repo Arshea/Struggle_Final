fc8fdd18b73abdf72f3fe8dd8b5ceabf8239ebe5
MMicklethwaite <m.micklethwaite@live.co.uk>
2016-11-26 01:24:59 -0800

    Light pickup narrations Jenga, bed, bookhouse assets

--- Assets/_Scripts/MusicManager.cs
+++ Assets/_Scripts/MusicManager.cs
@@ -55,39 +55,7 @@ struct Narration {
 	}
 }
 public class MusicManager : MonoBehaviour {
-	private string[] narrationFileNames = {
-		//DELETETHISLINE audioclipfilename,type
-		"vox_story_start_proc.wav",
-		/*"vox_light_pickup_01_proc.wav",
-		"vox_light_pickup_02_proc.wav",
-		"vox_light_pickup_03_proc.wav",
-		"vox_light_pickup_04_proc.wav",
-		"vox_story_end_proc.wav",*/
-
-		/*"vox_enemy_encounter_proc.wav",
-		"vox_puzzle_climbing_start.wav",
-		"vox_puzzle_climbing_end_proc.wav",
-		"vox_puzzle_domino_proc.wav",
-		"vox_puzzle_water_proc.wav",
-
-		"vox_wand_proc.wav",
-		"vox_boat_proc.wav",
-		"vox_chessboard_proc.wav",
-		"vox_clock_proc.wav",
-		"vox_domino_proc.wav",
-		"vox_earphones_proc.wav",
-		"vox_fathers_day_proc.wav",
-		"vox_house_of_cards_proc.wav",
-		"vox_paper_crane_proc.wav",
-		"vox_rubiks_proc.wav",
-		"vox_scrabble_proc.wav",
-		"vox_spinning_top_proc.wav",
-		"vox_sponge_proc.wav",*/
-
-		/*"vox_idle_01_proc.wav",
-		"vox_idle_02_proc.wav",
-		"vox_idle_03_proc.wav"*/
-	};
+	
 	private NarrationType[] types = {
 		NarrationType.PROGRESS,
 		NarrationType.PROGRESS,
@@ -155,45 +123,21 @@ public class MusicManager : MonoBehaviour {
 		//Assign all sources
 		main_source = GameObject.Find ("main_audio_source").GetComponent<AudioSource>();
 		ambient_source = GameObject.Find ("ambient_audio_source").GetComponent<AudioSource>();
-		narration_audio_source = GameObject.Find ("narration_audio_source").GetComponent<AudioSource>();;
-		num_of_narrations = narrationFileNames.Length;
+		narration_audio_source = GameObject.Find ("narration_audio_source").GetComponent<AudioSource>();
+		num_of_narrations = narration_clips.Length;
 		narrations = new Narration[num_of_narrations];
+
 		//Load all audio clips, with type and trigger for narration
 		for(int i = 0; i<num_of_narrations; i++) {
-			/*string fileName = "Assets/Audio/Narrations/" + narrationFileNames [i];
-			AudioClip narrationClip = Resources.Load<AudioClip>("Audio/Narrations/vox_story_start_proc");*/
-			/*if (narrationClip == null)
-				Debug.Log ("waaaah");
-				//Debug.Log ("STORY START: "+narrationClip.length);*/
-			Narration narration = new Narration (narration_clips[i], types [i], i);
+			Narration narration = new Narration (narration_clips[i], types[i], i);
 			narrations[i] = narration;
 
 		}
-		//Debug.Log ("STORY START: "+ narrations[0].clip);
-
-		//main_theme = (AudioClip)Resources.Load ("Audio/Music/main_theme_01.wav");
-		//light_variation = (AudioClip)Resources.Load ("Audio/Music/light_pickup_01a.wav");
 
 		/*When the game starts, it is quiet and the first narration plays*/
 		int index = searchNarrationByTrigger (ObjectTriggerType.STORY_START);
-		narration_audio_source.clip = narrations [index].clip;
-		narration_audio_source.loop = false;
-		narration_audio_source.volume = 1.0f;
-		narration_audio_source.Play();	
-		/*When the game starts the background music plays softly while the narrator speaks.*/
-		/*main_source.clip = main_theme;
-		main_source.loop = true;
-		ambient_source.Stop ();
-		main_source.volume = 0.5f;
-		StartCoroutine(audio_util.play_sound_fade_in(main_source,2,false));
-		//main_source.Play ();*/
+		playNarrationOfIndex (index);
 
-		/*Note 1: Would love to take away player control at this intro_clip. Maybe just a dark screen and her voice playing*/
-		/*narration_audio_source.clip = narration [current_narrative_index];
-		narration_audio_source.loop = false;
-		narration_audio_source.volume = 1.0f;
-		narration_audio_source.PlayDelayed (2);					//Sounds weird if not delayed
-		has_played_before[0] = true;*/
 
 	}
 
@@ -206,34 +150,13 @@ public class MusicManager : MonoBehaviour {
 		Debug.Log ("Could not find narration");
 		return -1;
 	}
-	/*void narration_transition_with_dependency (int previous_narration_index) {
-		if (has_played_before [previous_narration_index + 1] == false) {
-			if ((narration_audio_source.isPlaying == false) && (current_narrative_index == previous_narration_index)) {
-				current_narrative_index++;
-				narration_audio_source.clip = narration [current_narrative_index];
-				narration_audio_source.loop = false;
-				narration_audio_source.volume = 1.0f;
-				main_source.volume = 0.5f;
-				narration_audio_source.Play ();	
-			}
-		}
 
+	void playNarrationOfIndex(int index) {
+		narration_audio_source.clip = narrations [index].clip;
+		narration_audio_source.loop = false;
+		narration_audio_source.volume = 1.0f;
+		narration_audio_source.Play();
 	}
-
-	void narration_transition_without_dependency (int next_narration_index, int delayInSeconds) {
-		if (!has_played_before[next_narration_index]) {
-			if ((narration_audio_source.isPlaying == false) && (current_narrative_index != next_narration_index)) {
-				current_narrative_index = next_narration_index;
-				narration_audio_source.clip = narration [current_narrative_index];
-				narration_audio_source.loop = false;
-				narration_audio_source.volume = 1.0f;
-				main_source.volume = 0.5f;
-				narration_audio_source.PlayDelayed (delayInSeconds);	
-				has_played_before[next_narration_index] = true;
-			}
-		}
-	}*/
-
 
 	public void playLightPickupMusic() {
 
@@ -251,25 +174,25 @@ public class MusicManager : MonoBehaviour {
 
 	}
 
-	/*public void playLightPickupNarration(int lightNumber) {
+	public void playLightPickupNarration(int lightNumber) {
 		// Narration ---------------------------------------------------------------------
+		int index = 1;
 		if (lightNumber == 0) {
-			narration_transition_without_dependency (4,2); //Pass the narration index that you want to change it to
+			index = searchNarrationByTrigger (ObjectTriggerType.LIGHT_1); //Pass the narration index that you want to change it to
 		}
 
 		if (lightNumber == 1) {
-			narration_transition_without_dependency (6, 2);
-
+			index = searchNarrationByTrigger (ObjectTriggerType.LIGHT_2); //Pass the narration index that you want to change it to
 		}
 		if (lightNumber == 2) {
-			narration_transition_without_dependency (7, 2);
-			narration_transition_with_dependency (7);
+			index = searchNarrationByTrigger (ObjectTriggerType.LIGHT_3); //Pass the narration index that you want to change it to
 		}
 		if (lightNumber == 3) {
-			narration_transition_without_dependency (9, 2);
+			index = searchNarrationByTrigger (ObjectTriggerType.LIGHT_4); //Pass the narration index that you want to change it to
 			back_home_guide.SetActive (true);
 		}
-	}*/
+		playNarrationOfIndex(index);
+	}
 
 	void Update() {
 
@@ -313,5 +236,33 @@ public class MusicManager : MonoBehaviour {
 		}
 		
 	}
+
+/*void narration_transition_with_dependency (int previous_narration_index) {
+		if (has_played_before [previous_narration_index + 1] == false) {
+			if ((narration_audio_source.isPlaying == false) && (current_narrative_index == previous_narration_index)) {
+				current_narrative_index++;
+				narration_audio_source.clip = narration [current_narrative_index];
+				narration_audio_source.loop = false;
+				narration_audio_source.volume = 1.0f;
+				main_source.volume = 0.5f;
+				narration_audio_source.Play ();	
+			}
+		}
+
+	}
+
+	void narration_transition_without_dependency (int next_narration_index, int delayInSeconds) {
+		if (!has_played_before[next_narration_index]) {
+			if ((narration_audio_source.isPlaying == false) && (current_narrative_index != next_narration_index)) {
+				current_narrative_index = next_narration_index;
+				narration_audio_source.clip = narration [current_narrative_index];
+				narration_audio_source.loop = false;
+				narration_audio_source.volume = 1.0f;
+				main_source.volume = 0.5f;
+				narration_audio_source.PlayDelayed (delayInSeconds);	
+				has_played_before[next_narration_index] = true;
+			}
+		}
+	}*/
 
 }
