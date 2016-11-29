ba78ac58e0ae209774a3bf77727b2692887f2b9a
Arshea Bimal <bimal@usc.edu>
2016-11-28 16:04:11 -0800

    Carpet hair stuff New animations : Swan etc Three stack AI fixes
    ~ Martin

---
+++ Assets/New fixed animations 28 Nov/SWAN_FBXFILES/SWAN_FBXFILES/SwanAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwanAnimation : MonoBehaviour {
+
+	public float flightTime = 2.15f;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponentInParent<InteractionManager>().trigger_cooldown_time = flightTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void TriggerInteraction(){
+		GetComponent<Animator> ().SetTrigger ("IdleFly");
+		StartCoroutine ("swanFlying");
+	}
+
+	IEnumerator swanFlying() {
+		yield return new WaitForSeconds (2.0f);
+		//Debug.Log ("SwanAnimation:: Going back to the ground thanks I had fun");
+		GetComponent<Animator> ().SetTrigger ("FlyIdle");
+	}
+}
